dcd7359dac693d9fd2ad4fad84583796a83d8ccc
Petterson Pham <pnpham1@buffs.wtamu.edu>
2018-04-02 10:47:15 -0500

    Adding final version of homework

--- homework_6/UDPCli.py
+++ homework_6/UDPCli.py
@@ -5,14 +5,10 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_address = ('127.0.0.1', 12000)
 
 while True:
-    userdata = raw_input("Please input lowercase sentence")
-    sock.sendto(userdata, server_address)
+    userdata = raw_input("Please input lowercase sentence: ")
+    sock.sendto(userdata.encode(), server_address)
     data, server = sock.recvfrom(4096)
-    print(data)
-    number, server = sock.recvfrom(4096)
-    print(number)
-    count, server = sock.recvfrom(4096)
-    print(count)
+    print(data.decode())
 
 print("Closing socket.")
 sock.close()
--- homework_6/UDPSer.py
+++ homework_6/UDPSer.py
@@ -3,18 +3,18 @@ import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 sock.bind(('', 12000))
+message = 0
 
 print("The server is ready to receive...")
 while True:
     data, address = sock.recvfrom(4096)
-    print(data)
-    sendback = data.upper()
-    sendthistoo = str(len(data))
-    countwords  = data.split()
-    count = str(len(countwords))
+    rtnsender = data.decode()
+    upper = rtnsender.upper()
+    stringlength = str(len(rtnsender))
+    message+= 1
+    print("The received " + str(message)  +" from client is " + rtnsender + " the length of message is " + stringlength)
+    
+    sendback = "The number of characters in the inputted string " + upper + " is " + stringlength
 
     if data:
-        sent = sock.sendto(sendback, address)
-        sent = sock.sendto(sendthistoo, address)
-        sent = sock.sendto(count,address)
-        print("sending back data")
+        sent = sock.sendto(sendback.encode(), address)
